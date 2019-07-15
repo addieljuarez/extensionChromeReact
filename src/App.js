b16642d90987dc3319f1bcb78450bf5c690d3b80
Addiel <addielJuarez@gmail.com>
2019-07-14 23:50:12 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./content.css";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+
+  
+  
+
+
+class Main extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            url: 'https://gist.githubusercontent.com/addieljuarez/5518083/raw/14c5ecf04bae5cd873754dd5359efacb9dcd478c/gistfile1.json',
+            method: '',
+            response: '',
+
+        }
+    }
+
+
+    
+    
+    _onSelect(_result){
+
+    }
+
+    changeUrl(e){
+        this.setState({
+            url: e.target.value
+        });
+    }
+
+     sendRequest(){
+        const that = this;
+
+        if(this.state.url != ''){
+            fetch(this.state.url, {
+                method: 'GET',
+            })
+            .then(result => result.json())
+            .then(function(_data){
+                console.log(_data);
+                that.setState({
+                  response: JSON.stringify(_data)
+                });
+            })
+            .catch(function(error){
+                
+                that.setState({
+                  response: JSON.stringify(error)
+              });
+              console.log(error);
+            });
+                
+        }else{
+            alert('pon una url primero');
+        }
+        
+        
+    }
+
+    render() {
+
+        const options = [
+            'GET'
+        ];
+        const defaultOption = options[0];
+        return (
+            <div className={'my-extension'}>
+                {/* url */}
+                <input placeholder='url' value={this.state.url} onChange={this.changeUrl.bind(this)}></input>
+                <br></br>
+                {/* method */}
+                <Dropdown options={options} onChange={this._onSelect.bind(this)} value={defaultOption} placeholder="Select an option" />
+                <br></br>
+                {/* Content Type */}
+                <button onClick={this.sendRequest.bind(this)}>Request</button>
+                <br></br>
+                <h5>{this.state.response}</h5>
+            </div>
+        )
+    }
 }
 
-export default App;
+// const app = document.createElement('div');
+// // app.id = "extension-root";
+// document.body.appendChild(app);
+// ReactDOM.render(<Main />, app);
+
+export default Main;
--- src/content.js
+++ src/content.js
@@ -1,64 +1,94 @@
-/**
- * File: /Users/addielJuarez/Documents/developer/react/extension-chrome-react/src/content.js
- * Project: /Users/addielJuarez/Documents/developer/react/extension-chrome-react
- * Created Date: Sunday, July 14th 2019, 6:45:22 pm
- * Author: Addiel Juarez
- * -----
- * Last Modified: Sun Jul 14 2019
- * Modified By: Addiel Juarez
- * -----
- * Copyright (c) 2019 Addiel
- * ------------------------------------
- */
 
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Frame, { FrameContextConsumer }from 'react-frame-component';
 import "./content.css";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+
+  
+  
+
 
 class Main extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            url: 'https://gist.githubusercontent.com/addieljuarez/5518083/raw/14c5ecf04bae5cd873754dd5359efacb9dcd478c/gistfile1.json',
+            method: '',
+            response: '',
+
+        }
+    }
+
+
+    
+    
+    _onSelect(_result){
+
+    }
+
+    changeUrl(e){
+        this.setState({
+            url: e.target.value
+        });
+    }
+
+     sendRequest(){
+        const that = this;
+
+        if(this.state.url != ''){
+            fetch(this.state.url, {
+                method: 'GET',
+            })
+            .then(result => result.json())
+            .then(function(_data){
+                console.log(_data);
+                that.setState({
+                  response: JSON.stringify(_data)
+                });
+            })
+            .catch(function(error){
+                
+                that.setState({
+                  response: JSON.stringify(error)
+              });
+              console.log(error);
+            });
+                
+        }else{
+            alert('pon una url primero');
+        }
+        
+        
+    }
+
     render() {
+
+        const options = [
+            'GET'
+        ];
+        const defaultOption = options[0];
         return (
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}> 
-               <FrameContextConsumer>
-               {
-               // Callback is invoked with iframe's window and document instances
-                   ({document, window}) => {
-                      // Render Children
-                        return (
-                           <div className={'my-extension'}>
-                               <h1>Hello world - My first Extension</h1>
-                           </div>
-                        )
-                    }
-                }
-                </FrameContextConsumer>
-            </Frame>
+            <div className={'my-extension'}>
+                {/* url */}
+                <input placeholder='url' value={this.state.url} onChange={this.changeUrl.bind(this)}></input>
+                <br></br>
+                {/* method */}
+                <Dropdown options={options} onChange={this._onSelect.bind(this)} value={defaultOption} placeholder="Select an option" />
+                <br></br>
+                {/* Content Type */}
+                <button onClick={this.sendRequest.bind(this)}>Request</button>
+                <br></br>
+                <h5>{this.state.response}</h5>
+            </div>
         )
     }
 }
 
 const app = document.createElement('div');
-app.id = "my-extension-root";
-
+// app.id = "extension-root";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
-
-app.style.display = "none";
-
-chrome.runtime.onMessage.addListener(
-   function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action") {
-        toggle();
-      }
-   }
-);
-
-function toggle(){
-   if(app.style.display === "none"){
-     app.style.display = "block";
-   }else{
-     app.style.display = "none";
-   }
-}
